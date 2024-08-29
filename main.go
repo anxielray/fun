d7ca09f22145c8afdc8ae48c50bde6d6816580fd
rogwel <anxielworld@gmail.com>
2024-08-29 16:14:15 +0300

    [Add]: Add the raw source code without comments

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	println("What's your name ?")
+	//add a mini AI
+	var name string
+	fmt.Scanf("%s\n\n", &name)
+	fmt.Printf("Your name is %s\n", name)
+}
